7bf2614ba5dc9bbe15ccfa331bf0e1d37467018f
Aditya thakur <aadi30.thakur@gmail.com>
2020-09-05 00:04:00 +0530

    mongo onlinr

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { getUserById, getUser, updateUser,userPurchaseList } = require("../controllers/user");
+const {
+  getUserById,
+  getUser,
+  updateUser,
+  userPurchaseList,
+} = require("../controllers/user");
 const { isAdmin, isAuthenticated, isSignedIn } = require("../controllers/auth");
 
 router.param("userId", getUserById);
@@ -10,6 +15,11 @@ router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
 // rout/er.get("/users",getAllUsers);
 router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);
 
-router.get("/orders/user/:userId", isSignedIn, isAuthenticated, userPurchaseList);
+router.get(
+  "/orders/user/:userId",
+  isSignedIn,
+  isAuthenticated,
+  userPurchaseList
+);
 
 module.exports = router;
