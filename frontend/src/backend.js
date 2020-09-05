bfcf2a2b57969734747d27a8536be1a00aed845a
Aditya thakur <aadi30.thakur@gmail.com>
2020-09-05 12:35:21 +0530

    added to production

--- frontend/src/backend.js
+++ frontend/src/backend.js
@@ -1,1 +1,4 @@
-export const API = process.env.REACT_APP_BACKEND;
+export const API =
+  process.env.REACT_APP_BACKEND === "production"
+    ? "https://tees-store.herokuapp.com/api"
+    : "http://localhost:8000/api";
