d037ce13a92d81e7ce6eb399014401abeab49b44
Aditya thakur <aadi30.thakur@gmail.com>
2021-02-04 15:15:23 +0530

    signinpage updated

--- frontend/src/user/Signin.js
+++ frontend/src/user/Signin.js
@@ -5,7 +5,7 @@ import { Redirect } from "react-router-dom";
 import { signin, authenticate, isAuthenticated } from "../auth/helper";
 const Signin = () => {
   const [values, setValues] = useState({
-    email: "welcome@oyo.com",
+    email: "welcome@toMywebsite.com",
     password: "12345",
     error: "",
     loading: false,
