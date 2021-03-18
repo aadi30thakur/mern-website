609a45e95dda0b5452bb716a239da49c34dc6cd2
Aditya thakur <aadi30.thakur@gmail.com>
2021-03-18 13:29:15 +0530

    fixed bugs

--- controllers/payment.js
+++ controllers/payment.js
@@ -1,7 +1,8 @@
 const braintree = require("braintree");
 
 const gateway = new braintree.BraintreeGateway({
-    environment: braintree.Environment.Sandbox,
+    // environment: braintree.Environment.Sandbox,
+    environment: braintree.Environment.Production,
     merchantId: "9pnqmgt6sd5w8y34",
     publicKey: "td4tggcvp8gpxn4j",
     privateKey: "fca98b6fbb7355c6fe3a8886c72423a1"
